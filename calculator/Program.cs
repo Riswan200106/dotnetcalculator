25412653b0da20a50e964f42d0108bdf1ba4d3be
Riswan200106 <riswanrichu621@gmail.com>
2024-11-28 19:05:29 +0530

    Add project files.

---
+++ calculator/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Simple Calculator!");
+            Console.WriteLine("----------------------------------");
+
+            Console.Write("Enter the first number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter an operator (+, -, *, /): ");
+            string operation = Console.ReadLine();
+
+            Console.Write("Enter the second number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            double result = 0;
+
+            if (operation == "+")
+            {
+                result = num1 + num2;
+            }
+            else if (operation == "-")
+            {
+                result = num1 - num2;
+            }
+            else if (operation == "*")
+            {
+                result = num1 * num2;
+            }
+            else if (operation == "/")
+            {
+                if (num2 != 0)
+                {
+                    result = num1 / num2;
+                }
+                else
+                {
+                    Console.WriteLine("Error: Division by zero is not allowed.");
+                    return;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid operator. Please try again.");
+                return;
+            }
+
+            Console.WriteLine($"Result: {num1} {operation} {num2} = {result}");
+            Console.WriteLine("Thank you for using the calculator!");
+        }
+    }
+
+}
